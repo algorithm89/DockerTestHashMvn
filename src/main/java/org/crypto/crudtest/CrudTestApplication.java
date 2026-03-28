5e72e734affc6d0d8ffc0143312b23857c6f333c
algorithm89 <alexei.go89@gmail.com>
2026-03-28 19:41:01 -0400

    wow

--- src/main/java/org/crypto/crudtest/CrudTestApplication.java
+++ src/main/java/org/crypto/crudtest/CrudTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTestApplication.class, args);
+    }
 
 }
---
+++ src/main/java/org/crypto/crudtest/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package org.crypto.crudtest.repository;
+
+import org.crypto.crudtest.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
