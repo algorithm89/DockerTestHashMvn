5ee39bf16d9204f4ac2cc62690bec67ba71fe478
algorithm89 <alexei.go89@gmail.com>
2026-04-02 20:33:50 -0400

    spring.datasource.url=jdbc:mysql://${DB_HOST:localhost}:3306/testdb

--- src/main/java/org/crypto/crudtest/DebugConfig.java
+++ src/main/java/org/crypto/crudtest/DebugConfig.java
@@ -16,13 +16,20 @@ public class DebugConfig {
     @Value("${spring.datasource.password}")
     private String password;
 
+    @Value("${DB_HOST:localhost}")
+    private String dbHost;
+
+    @Value("${VAULT_HOST:127.0.0.1}")
+    private String vaultHost;
+
     @PostConstruct
     public void printConfig() {
         System.out.println("=== DB CONFIG DEBUG ===");
-        System.out.println("DB URL  = " + url);
-        System.out.println("DB USER = " + username);
-        System.out.println("DB PASS = " + password);
+        System.out.println("DB_HOST  = " + dbHost);
+        System.out.println("DB URL   = " + url);
+        System.out.println("DB USER  = " + username);
+        System.out.println("DB PASS  = " + password);
+        System.out.println("VAULT_HOST = " + vaultHost);
         System.out.println("=======================");
     }
 }
-
